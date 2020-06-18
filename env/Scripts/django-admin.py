52a3027906bc315edffd2cd17273b0a56ad9c2b4
choudharyamit26 <ravichoudhary766@gmail.com>
2020-06-18 23:15:35 +0530

    Updated Functionalities

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Choudhary's\Desktop\python\GST-MANAGER-master\GST-MANAGER-master\Gstmanager\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
