673bdbabe949b8b3a49f4a74f70a1a4b8628b842
choudharyamit26 <45892851+choudharyamit26@users.noreply.github.com>
2020-06-21 21:35:41 +0530

    Add files via upload
    Updated Views for Dynamic fields in Modelform

--- src/forms.py
+++ src/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from django.forms import modelformset_factory,inlineformset_factory
+from django.forms import modelformset_factory, inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Bill, Product, Client, OrderItem
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(
+        max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
@@ -19,11 +20,18 @@ class OrderItemForm(forms.ModelForm):
 
 
 class BillForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        self.fields['buyer'].queryset = Client.objects.filter(user=user)
+
     class Meta:
         model = Bill
         fields = ('buyer', 'buyer_address', 'buyer_gst')
-        # fields = '__all__'
-BillFormSet = modelformset_factory(OrderItem, fields=('product','quantity','gst','price','hsn_code'))
+
+BillFormSet = modelformset_factory(OrderItem, fields=(
+    'product', 'quantity', 'gst', 'price', 'hsn_code'))
+
 
 class ClientForm(forms.ModelForm):
     class Meta:
@@ -34,7 +42,8 @@ class ClientForm(forms.ModelForm):
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('product_name', 'product_company', 'product_model', 'quantity', 'price', 'hsn_code')
+        fields = ('product_name', 'product_company',
+                  'product_model', 'quantity', 'price', 'hsn_code')
 
 
 class BillUpdateForm(forms.ModelForm):
